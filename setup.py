41c1df5f84b4e7d0be9675105891f76b76209f02
Philip Basford <pjb1u12@soton.ac.uk>
2019-01-29 16:37:04 +0000

    name now lower case to match others

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Sensirion SPS030",
-    version="0.0.1",
+    name="sensirion sps030",
+    version="0.0.2",
     author="Florentin Bulot",
     author_email="f.bulot@soton.ac.uk",
     description="An interface for Sensirion SPS030 particulate matter sensors",
