64016231b28fe97675718da619fe4c9b0315fda9
FlorentinBulotUoS <fb1n17@soton.ac.uk>
2019-11-15 14:49:10 +0000

    Fixed test.py and added arguments handling

--- test.py
+++ test.py
@@ -1,16 +1,43 @@
 import sensirion_sps030
 from time import sleep
+from argparse import ArgumentParser
+import logging
 
+def test(sensor_port, console_log_level):
+    RX_DELAY_S = 0.02 
 
-RX_DELAY_S = 0.02 
+    SPS030 = sensirion_sps030.Sensirion(
+    	port=sensor_port, log_level=console_log_level)
 
-SPS030 = sensirion_sps030.Sensirion(
-	port="/dev/ttyUSB_SPS030-60762402-3", log_level="DEBUG")
+    sleep(RX_DELAY_S)
+    mess=SPS030.read_measurement()
 
-SPS030._start_measurement()
-SPS030._tx(b'\x00', b'\x03')
-sleep(RX_DELAY_S)
-mess=SPS030.serial.read(30)
+    SPS030.logger.info("reading: %s",mess)
 
-print(mess)
+if __name__ == "__main__":
+    ARG_PARSER = ArgumentParser(
+        description="Return one reading from the SPS030 sensor attached to the speficied serial port")
+    LOGGING_OUTPUT = ARG_PARSER.add_mutually_exclusive_group()
+    LOGGING_OUTPUT.add_argument(
+        "-q",
+        "--quiet",
+        action="store_true",
+        help="Suppress most ouput")
+    LOGGING_OUTPUT.add_argument(
+         "-v",
+         "--verbose",
+         action="store_true",
+         help="Maximum verbosity output on command line")
+    ARG_PARSER.add_argument(
+       "sensor_port",
+        action="store",
+        help="Port to which the sensor is connected")
+    ARGS = ARG_PARSER.parse_args()
+    CONSOLE_LOG_LEVEL = logging.INFO
+    if ARGS.quiet:
+        CONSOLE_LOG_LEVEL = logging.ERROR
+    elif ARGS.verbose:
+        CONSOLE_LOG_LEVEL = logging.DEBUG
+    
+    test(ARGS.sensor_port, CONSOLE_LOG_LEVEL)
 
